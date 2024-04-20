6beb984b300c306c146146b66a9e43dd07ab77d1
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-04-20 22:26:15 +0500

    navbar error fixed

--- client/src/components/navbar-menu.tsx
+++ client/src/components/navbar-menu.tsx
@@ -4,8 +4,9 @@ import * as React from "react"
 import { Link } from '../navigation';
 import { usePathname } from "next/navigation";
 import { NavbarType } from "../../interfaces/navbar-interface";
-
-
+function isNavbarHref(href: string): href is "/products" | "/" | "/contacts" {
+    return href === "/products" || href === "/" || href === "/contacts";
+}
 export function NavbarMenu({ navItems, locale }: { navItems: NavbarType[], locale: string }) {
     const pathname = usePathname();
     return (
@@ -13,11 +14,17 @@ export function NavbarMenu({ navItems, locale }: { navItems: NavbarType[], local
             <ul className="gap-6 flex flex-col md:flex-row">
                 {navItems.map((item) => (
                     <li key={item.name} className={`border-b-2 ${pathname == `/${locale}${item.href === "/" ? "" : item.href}` ? "border-[#52A742]" : "border-transparent"} hover:border-b-2 hover:border-[#52A742] transition`}>
-                        <Link href={item.href}>
+                        {isNavbarHref(item.href) ? (
+                            <Link href={item.href}>
+                                <p className="font-medium text-base">
+                                    {item.name}
+                                </p>
+                            </Link>
+                        ) : (
                             <p className="font-medium text-base">
                                 {item.name}
                             </p>
-                        </Link>
+                        )}
                     </li>
                 ))}
             </ul>
