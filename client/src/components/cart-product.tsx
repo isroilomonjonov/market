f0caab2b2ae07db840da4076f9b984c9a31fe523
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-04-21 21:18:31 +0500

    some changes in cart

--- client/src/components/cart-product.tsx
+++ client/src/components/cart-product.tsx
@@ -7,12 +7,12 @@ const CartProduct = ({ el }: { el: any }) => {
     return (
         <div className='flex gap-16 items-center flex-wrap md:flex-nowrap mb-5'>
             <img className="w-64" src={el.image} alt={el.title} />
-            <div className='flex gap-4 flex-col dark:border-b md:border-none pb-4 rounded justify-between'>
-                <div className='flex flex-col gap-4'>
+            <div className='flex gap-4 flex-col items-center dark:border-b md:border-none pb-4 rounded justify-between'>
+                <div className='flex items-center flex-col gap-4'>
                     <h3 className="text-2xl sm:text-3xl">{el.title}</h3>
                     <p className="text-gray-500 text-lg sm:text-xl">{el.description}</p>
                 </div>
-                <div className='flex gap-4'>
+                <div className='flex  items-center justify-center gap-4'>
                     <p className="text-xl sm:text-2xl">${el.price}</p>
                     <AddAndRemoveBtn el={el} />
                     <p className="text-2xl sm:text-3xl">${Math.ceil(el.quantity * el.price)}</p>
