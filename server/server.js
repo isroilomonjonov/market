a92da767fc72c5af9d84085694f5e02fe1e3a492
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-05-01 20:19:00 +0500

    backend base code

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const db = require("./utils/db");
+const app = require("./app");
+const PORT = process.env.PORT || 8080;
+/**
+ * Starts the server and listens on the specified port.
+ *
+ * @return {Promise<void>} - A promise that resolves when the server starts successfully.
+ */
+const start = async () => {
+  try {
+    await db.authenticate();
+    await db.sync({
+      // force: true,
+      // alter: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server ${process.env.NODE_ENV} started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
