ba566fbc4e301891b331cd84c8f87c9a240a0e3b
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-05-01 20:02:52 +0500

    front full

--- client/src/components/cart-product-container.tsx
+++ client/src/components/cart-product-container.tsx
@@ -5,13 +5,14 @@ import { CircleArrowRight } from 'lucide-react';
 
 const CartProductContainer = () => {
     const products = useAppSelector((state) => state.auth.products);
+    const totalPrice = useAppSelector((state) => state.auth.totalPrice);
     return (
         <div className="pt-12">
             <h2 className="text-2xl font-bold mb-4">Cart</h2>
             {products.map((product: any) => (
                 <CartProduct key={product.id} el={product} />
             ))}
-            {products.length > 0 && <button className='flex gap-2 p-4 px-8 border'>Buyurtma berish <CircleArrowRight className='w-6 h-6' /></button>}
+            {products.length > 0 && <button className='flex gap-2 p-4 px-8 border items-center'>Buyurtma berish <CircleArrowRight className='w-6 h-6' /><span className='text-2xl'>${totalPrice}</span></button>}
         </div>
     )
 }
