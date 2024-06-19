0c9a3152b981fcc5c97ebd4a295ce5ad675b72d9
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-06-19 15:42:02 +0500

    some changes front

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const serviceURL = "http://localhost:9090/api/v1";
-export const imageURL = "http://localhost:9090/img/";
+// export const serviceURL = "http://localhost:9090/api/v1";
+export const serviceURL = "https://market-production-c362.up.railway.app/api/v1";
+// export const imageURL = "http://localhost:9090/img/";
+export const imageURL = "https://market-production-c362.up.railway.app/img/";
 const axiosInstance = axios.create({
   baseURL: serviceURL,
   // baseURL: "https://omontech-server.uz/api/v1/",
